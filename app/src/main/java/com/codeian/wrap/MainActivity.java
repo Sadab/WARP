048c8a7b468d576fa424347af88993d75b862993
Sadab <sadabrahman@gmail.com>
2021-03-28 09:20:17 +0600

    Added Splash

--- app/src/main/java/com/codeian/wrap/MainActivity.java
+++ app/src/main/java/com/codeian/wrap/MainActivity.java
@@ -2,7 +2,9 @@ package com.codeian.wrap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, StatusActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
     }
 }
