fe7626262afa1f42c3916179e1930680a14797b9
Sadab <sadabrahman@gmail.com>
2021-03-28 10:01:07 +0600

    Added checker to check if the app is running for the first time

--- app/src/main/java/com/codeian/wrap/MainActivity.java
+++ app/src/main/java/com/codeian/wrap/MainActivity.java
@@ -13,13 +13,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, StatusActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }, 2000);
+        Boolean firstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE).getBoolean("firstRun", true);
+
+        if(firstRun == false) {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(MainActivity.this, StatusActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, 2000);
+        } else {
+            getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit().putBoolean("firstRun", false).commit();
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }, 2000);
+        }
+
+
     }
 }
