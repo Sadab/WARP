300ebac7398977501691a75f66e8e4c756345289
Sadab <sadabrahman@gmail.com>
2021-03-28 08:28:47 +0600

    Initial commit

--- app/src/main/java/com/codeian/wrap/StatusActivity.java
+++ app/src/main/java/com/codeian/wrap/StatusActivity.java
@@ -2,52 +2,88 @@ package com.codeian.wrap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
 
 import io.github.krtkush.lineartimer.LinearTimer;
 import io.github.krtkush.lineartimer.LinearTimerView;
 
-public class StatusActivity extends AppCompatActivity {
+public class StatusActivity extends AppCompatActivity implements LinearTimer.TimerListener {
     LinearTimerView linearTimerView;
-    ImageView startBtn;
+    LinearTimer linearTimer;
+    Button startBtn;
+    TextView timeRemaining;
+    ImageView settingIcon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
 
+        long duration = 10 * 1000;
         linearTimerView = (LinearTimerView) findViewById(R.id.timer);
         startBtn = findViewById(R.id.startBtn);
-        linearTimerView.setProgressColor(Color.parseColor("#E23369"));
+        timeRemaining = findViewById(R.id.timerTxt);
+        settingIcon = findViewById(R.id.settingIcon);
 
-        LinearTimer linearTimer = new LinearTimer.Builder()
+        linearTimer = new LinearTimer.Builder()
                 .linearTimerView(linearTimerView)
-                .duration(10 * 1000)
+                .duration(duration)
+                .timerListener(this)
+                .getCountUpdate(LinearTimer.COUNT_DOWN_TIMER, 1000)
                 .build();
 
         // Start the timer.
         findViewById(R.id.startBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                linearTimer.startTimer();
+                try {
+                    linearTimer.startTimer();
+                } catch (IllegalStateException e) {
+                    Toast.makeText(StatusActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+
             }
         });
 
-        // Reset the timer
-        findViewById(R.id.timer).setOnClickListener(new View.OnClickListener() {
+        settingIcon.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                try {
-                    linearTimer.resetTimer();
-                } catch (IllegalStateException e) {
-                    e.printStackTrace();
-                    Toast.makeText(StatusActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
+            public void onClick(View view) {
+                Intent intent = new Intent(StatusActivity.this, SettingsActivity.class);
+                startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void animationComplete() {
+        Log.i("Animation", "animationComplete");
+        linearTimer.resetTimer();
+    }
+
+    @Override
+    public void timerTick(long tickUpdateInMillis) {
+        Log.i("Time left", String.valueOf(tickUpdateInMillis));
+        String formattedTime = String.format("%02d",
+                TimeUnit.MILLISECONDS.toSeconds(tickUpdateInMillis)
+                        - TimeUnit.MINUTES
+                        .toSeconds(TimeUnit.MILLISECONDS.toHours(tickUpdateInMillis)));
+        timeRemaining.setText(formattedTime + "s");
+        startBtn.setEnabled(false);
+    }
+
+    @Override
+    public void onTimerReset() {
+        timeRemaining.setText("");
+        startBtn.setEnabled(true);
     }
 }
 
