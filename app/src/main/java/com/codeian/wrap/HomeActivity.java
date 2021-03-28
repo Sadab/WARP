76a6fd99179d6f46f3f5f3fc72b7914dd5365723
Sadab <sadabrahman@gmail.com>
2021-03-28 10:06:36 +0600

    Added necessary stuffs

--- app/src/main/java/com/codeian/wrap/HomeActivity.java
+++ app/src/main/java/com/codeian/wrap/HomeActivity.java
@@ -2,13 +2,35 @@ package com.codeian.wrap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
-
+    Button settingsBtn;
+    ImageView settingsIcon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        settingsBtn = findViewById(R.id.settingsBtn);
+        settingsIcon = findViewById(R.id.settingsIcon);
+
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this, SettingsActivity.class));
+            }
+        });
+
+        settingsIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(HomeActivity.this, SettingsActivity.class));
+            }
+        });
     }
 }
